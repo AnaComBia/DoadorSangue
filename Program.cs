9b5f33aeca567fe6a25eab6bef4c71ba82aea2b6
AnaComBia <anabeatrizetorres@gmail.com>
2021-10-24 20:10:38 -0300

    finaliza programa Doar Sangue

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DoadorSangue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n---Você Pode Doar Sangue---\n");
+
+            Console.Write("Indique sua idade: ");
+            double idade = Convert.ToDouble(Console.ReadLine());
+
+            if (idade < 18 || idade > 67)
+            {
+                Console.Write($"\nVocê não pode doar sangue com {idade} anos \n");
+            }
+            else
+            {
+               Console.Write($"\nVocê pode doar sangue com {idade} anos\n");
+            }
+        }
+    }
+}
